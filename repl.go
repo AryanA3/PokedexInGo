bfee5ca0c059dde28182514a1e38a8a525aa0abd
AryanA3 <aryanagarkar09@gmail.com>
2025-02-08 18:15:16 +0530

    Map command added

--- repl.go
+++ repl.go
@@ -5,31 +5,36 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/AryanA3/PokedexInGo/internal/pokeapi"
 )
 
-func startRepl() {
-	reader := bufio.NewScanner(os.Stdin)
-	var inp string
-	var cleaned_inp []string
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
 
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
-		inp = reader.Text()
-		cleaned_inp = cleanInput(inp)
 
-		if len(cleaned_inp) == 0 {
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
 			continue
 		}
 
-		cmd, ok := getCommands()[cleaned_inp[0]]
-		if ok {
-			err := cmd.callback()
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
 			continue
-
 		} else {
 			fmt.Println("Unknown command")
 			continue
@@ -38,15 +43,15 @@ func startRepl() {
 }
 
 func cleanInput(text string) []string {
-	text = strings.ToLower(text)
-	text = strings.TrimSpace(text)
-	return strings.Split(text, " ")
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -55,6 +60,16 @@ func getCommands() map[string]cliCommand {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
 		},
 		"exit": {
 			name:        "exit",
